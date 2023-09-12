3a4cb923da1d7f537341fae68183dfee6e0e1ad6
Ashcode <akshaycodeur@gmail.com>
2023-09-12 16:04:42 -0400

    Front page layout complete

--- theme/front-page.php
+++ theme/front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<section id="hero" class="relative py-10 md:py-20 flex items-center justify-center h-[70vh] bg-no-repeat bg-cover text-center" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/slide1.png) ;">
+<section id="hero" class="relative py-10 md:py-20 flex items-center justify-center h-[70vh] bg-no-repeat bg-cover text-center" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/slide2.png) ;">
     <div class="centrally p-5">
         <h2 class="block font-bold text-white text-3xl md:text-[2.5rem] leading-[1.2] text-center">SUPPORT - PARTNERSIP - GROWTH</h2>
         <p class="text-xl font-bold text-white">We help you put your customers at the center of your leadership, strategy, and operation.</p>
@@ -11,19 +11,15 @@
             <h2 class="text-2xl mb-5">ARE WE THE RIGHT FIT FOR YOU ?</h2>
             <p class="text-white mb-5">Would you like to arrange a meeting to talk to us about your situation and objectives? During a 30-minute no commitment interview, we will study your particular needs and the best way to achieve the results you are pursuing.</p>
 
-            <a href="#" target="_blank" class="w-full sm:w-auto inline-flex justify-center items-center text-center bg-theme-navy hover:bg-bg-theme-royal border border-transparent text-base md:text-lg text-white font-medium focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-5 tracking-wideflex-col">Contact Us </a>
+            <a href="#" target="_blank" class="w-full sm:w-auto inline-flex justify-center items-center text-center bg-theme-navy hover:bg-bg-theme-royal border border-transparent text-base md:text-lg text-white font-medium focus:ring-2 focus:ring-blue-600   transition py-3 px-5 tracking-wideflex-col">Contact Us </a>
 
         </div>
     </div>
 </section>
 
-<section id="aboutUs" class="relative bg-theme-navy overflow-hidden pt-10 md:pt-0">
-    <div class="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
-        <img src="<?php echo get_template_directory_uri() ?>/assets/img/solutions.webp" alt="Jeff Walker" class="h-56 w-full object-cover object-center-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none hidden lg:block">
-        <img src="../../assets/img//assets/img/solutions.webp" alt="Jeff Walker" class="h-56 w-full object-cover object-right-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none block lg:hidden">
-    </div>
+<section id="helpClients" class="relative bg-theme-navy overflow-hidden pt-10 md:pt-0">
+
     <div class="container mx-auto flex">
-        <div class="w-full w-0 lg:w-1/2"></div>
         <div class="relative z-10 bg-theme-blue w-full lg:w-1/2">
             <div class="py-12 lg:py-16 max-w-xl mx-auto pl-0 lg:pl-10" data-gtm-vis-recent-on-screen-32410801_439="305" data-gtm-vis-first-on-screen-32410801_439="305" data-gtm-vis-total-visible-time-32410801_439="5000" data-gtm-vis-has-fired-32410801_439="1">
                 <div class="sm:text-center lg:text-left">
@@ -38,8 +34,113 @@
                 </div>
             </div>
         </div>
+        <div class="w-full w-0 lg:w-1/2"></div>
     </div>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/solutions.webp" alt="" class="h-56 w-full object-cover object-center-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none hidden lg:block">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/solutions.webp" alt="" class="h-56 w-full object-cover object-right-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none block lg:hidden">
+    </div>
+</section>
 
+<section class="py-12 md:py-16" id="growth">
+    <div class="container mx-auto">
+        <div class="container mx-auto mb-12 md:mb-16 md:flex md:space-x-12 items-center">
+            <div class="md:w-1/2">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/img/growth.webp" alt="" class="h-96 w-full object-cover object-left-top md:h-96 lg:w-full lg:h-full">
+            </div>
+            <div class="md:w-1/2">
+                <div class="space-y-4">
+                    <p>In today's dynamic business environment, the integration of various functions, along with digital transformation and the right human resources, is a strategic game-changer. MINDSET champions this holistic approach through our specialised services.</p>
+                    <p>MINDSET is here to enhance your existing value proposition with solutions tailored to your unique needs. We add our expertise in various business domains, working closely with your team to provide a seamless, transparent, and accountable extension to your operations.</p>
+                    <p>Our services are designed to integrate smoothly with your business, offering a comprehensive solution that fosters growth and success across all sectors.</p>
+                    <p>We align objectives, streamline processes, and enhance strategies, leveraging digital transformation to unlock your business's full potential</p>
+                    <p>With MINDSET, you acquire a strategic partner committed to fostering collaboration, driving innovation, and propelling your organisation towards unified success.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="adaptFuture" class="relative bg-theme-navy overflow-hidden pt-10 md:pt-0">
+    <div class="container mx-auto flex">
+        <div class="relative z-10 bg-theme-blue w-full lg:w-1/2">
+            <div class="py-12 lg:py-16 max-w-xl mx-auto pl-0 lg:pl-10">
+                <div class="sm:text-center lg:text-left">
+                    <div class="relative">
+                        <h2 class="block font-bold text-slate-50 text-3xl md:text-[2.35rem] leading-[1.2] mb-4">ADAPT TO THE FUTURE</h2>
+                        <div class="mb-8 space-y-4 text-white">
+                            <p>Embracing digital strategies is no longer optional; it's a vital path to accelerate growth and maintain competitive edge.</p>
+                            <p>Modern customers demand unique, seamless experiences. Traditional sales and marketing techniques hold value, but digital transformation offers unparalleled opportunities. Utilizing tools like data analytics, automation, immersive content, and personalized messaging, digital strategies enable businesses to connect with audiences more effectively and efficiently.</p>
+                            <p>At MINDSET, we specialise in pinpointing opportunities for digital growth tailored to your industry. We craft custom strategies, seamlessly aligned with your business goals, to elevate sales and marketing to new heights</p>
+                            <p>Leveraging extensive expertise in digital trends and cutting-edge technologies, we confidently guide you through the complex digital landscape. Trust MINDSET as your partner in digital transformation, and together, we'll forge a pathway to continued success and innovation in your market.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-full w-0 lg:w-1/2"></div>
+    </div>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/vision.webp" alt="" class="h-56 w-full object-cover object-center-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none hidden lg:block">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/vision.webp" alt="" class="h-56 w-full object-cover object-right-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none block lg:hidden">
+    </div>
+</section>
+
+<section class="py-12 md:py-16" id="growth">
+    <div class="container mx-auto text-center">
+        <h2 class="block font-bold text-slate-800 text-3xl md:text-[2.35rem] leading-[1.2] mb-4">SELECTION OF SOLUTIONS</h2>
+        <p>We Provide tailor-made strategies that enhance your value proposition, diriving growth and revenue in an era of conscious consumerism.</p>
+        <p>Our flexible structure allows us to provide solutions completely tailoring the service to your clientele. With your brand at the forefront, our expertise ans services act as a complementary tool for revenue generation and business growth.</p>
+    </div>
+    <div class="container mx-auto">
+        <div class="max-w-screen-lg mx-auto">
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 responsive-grid">
+                <!-- Item 1 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 1 -->
+                </div>
+
+                <!-- Item 2 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 2 -->
+                </div>
+
+                <!-- Item 3 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 3 -->
+                </div>
+
+                <!-- Item 4 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 4 -->
+                </div>
+
+                <!-- Item 5 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 5 -->
+                </div>
+
+                <!-- Item 6 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 6 -->
+                </div>
+
+                <!-- Item 7 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 7 -->
+                </div>
+
+                <!-- Item 8 -->
+                <div class="bg-white p-4 shadow-md">
+                    <!-- Content for item 8 -->
+                </div>
+
+
+            </div>
+        </div>
+
+
+    </div>
 </section>
 
 <?php require 'footer.php'; ?>
--- theme/header.php
+++ theme/header.php
@@ -19,6 +19,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="icon" href="<?php echo get_template_directory_uri() ?>/assets/img/strategicmindseticon.png" type="image/x-icon">
 	<?php wp_head(); ?>
 </head>
 
