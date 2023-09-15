7c042d20b362d34cdf9518292e8f46d54960f0fa
Ashcode <akshaycodeur@gmail.com>
2023-09-15 11:27:02 -0400

    chore: corporate page about and solutions section update

--- theme/front-page.php
+++ theme/front-page.php
@@ -5,15 +5,17 @@
     }
 </style>
 <section id="hero" class="relative py-10 md:py-20 flex items-center justify-center h-[70vh] bg-no-repeat bg-cover text-center" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/human.jpg) ;">
-    <div class="centrally py-8 px-5 bg-slate-800 bg-opacity-70">
+    <div class="centrally py-8 px-5 bg-slate-800 bg-opacity-80 max-w-3xl">
         <h2 class="block font-bold text-white text-3xl md:text-[2.5rem] leading-[1.2] text-center">SUPPORT - PARTNERSIP - GROWTH</h2>
         <p class="text-xl mt-5 text-white">We help you put your customers at the center of your leadership, strategy, and operation.</p>
-        <a href="#contactUs" class="mt-10 w-full sm:w-auto inline-flex justify-center items-center text-center bg-white hover:bg-theme-navy border border-transparent text-base md:text-lg text-theme-navy hover:text-white font-medium focus:ring-2 focus:ring-blue-600   transition py-3 px-5 tracking-wideflex-col">Contact Us </a>
+        <h2 class="text-2xl mt-5 font-black text-white">ARE WE THE RIGHT FIT FOR YOU ?</h2>
+        <p class="text-white mb-5">Would you like to arrange a meeting to talk to us about your situation and objectives? During a 30-minute no commitment interview, we will study your particular needs and the best way to achieve the results you are pursuing.</p>
+        <a href="#contactUs" class=" w-full sm:w-auto inline-flex justify-center items-center text-center bg-white hover:bg-theme-navy border border-transparent text-base md:text-lg text-theme-navy hover:text-white font-medium focus:ring-2 focus:ring-blue-600   transition py-3 px-5 tracking-wideflex-col">Contact Us </a>
     </div>
 
     <div class="hidden absolute bottom-0 bg-slate-700 p-5 w-full bg-opacity-50" style="background-color: ;">
         <div class="text-white max-w-2xl mx-auto">
-            <h2 class="text-2xl mb-5">ARE WE THE RIGHT FIT FOR YOU ?</h2>
+
             <p class="text-white mb-5">Would you like to arrange a meeting to talk to us about your situation and objectives? During a 30-minute no commitment interview, we will study your particular needs and the best way to achieve the results you are pursuing.</p>
 
             <a href="#" class="w-full sm:w-auto inline-flex justify-center items-center text-center bg-theme-navy hover:bg-bg-theme-royal border border-transparent text-base md:text-lg text-white font-medium focus:ring-2 focus:ring-blue-600   transition py-3 px-5 tracking-wideflex-col">Contact Us </a>
@@ -22,22 +24,7 @@
     </div>
 </section>
 
-<section class="py-12 md:py-16" id="growth">
-    <div class="container mx-auto">
-        <div class="container mx-auto md:flex  items-center">
-            <div class="text-white max-w-2xl mx-auto text-center">
-                <h2 class="text-2xl mb-5 text-theme-navy">ARE WE THE RIGHT FIT FOR YOU ?</h2>
-                <p class="text-slate-700 mb-5">Would you like to arrange a meeting to talk to us about your situation and objectives? During a 30-minute no commitment interview, we will study your particular needs and the best way to achieve the results you are pursuing.</p>
-
-                <a href="#contactUs" class="w-full sm:w-auto inline-flex justify-center items-center text-center bg-theme-navy hover:bg-slate-700 border border-transparent text-base md:text-lg text-white font-medium focus:ring-2 focus:ring-blue-600   transition py-3 px-5 tracking-wideflex-col">Contact Us </a>
-
-            </div>
-        </div>
-    </div>
-</section>
-
-
-<section id="aboutU" class="relative bg-theme-navy overflow-hidden pt-10 md:pt-0 flex-col-reverse flex">
+<section id="aboutUs" class="relative bg-theme-navy overflow-hidden pt-10 md:pt-0 flex-col-reverse flex">
     <div class="container mx-auto flex">
         <div class="relative z-10 bg-theme-blue w-full lg:w-1/2">
             <div class="py-12 lg:py-16 max-w-xl mx-auto pl-0 lg:pl-10" data-gtm-vis-recent-on-screen-32410801_439="305" data-gtm-vis-first-on-screen-32410801_439="305" data-gtm-vis-total-visible-time-32410801_439="5000" data-gtm-vis-has-fired-32410801_439="1">
@@ -61,22 +48,30 @@
     </div>
 </section>
 
-<section class="py-12 md:py-16" id="ourProcess">
-    <div class="container mx-auto">
-        <div class="container mx-auto mb-12 md:mb-16 md:flex md:space-x-12 items-center">
-            <div class="md:w-1/2">
-                <img src="<?php echo get_template_directory_uri() ?>/assets/img/growth.webp" alt="" class="h-96 w-full object-cover object-left-top md:h-96 lg:w-full lg:h-full">
-            </div>
-            <div class="md:w-1/2">
-                <div class="space-y-4">
-                    <p>In today's dynamic business environment, the integration of various functions, along with digital transformation and the right human resources, is a strategic game-changer. MINDSET champions this holistic approach through our specialised services.</p>
-                    <p>MINDSET is here to enhance your existing value proposition with solutions tailored to your unique needs. We add our expertise in various business domains, working closely with your team to provide a seamless, transparent, and accountable extension to your operations.</p>
-                    <p>Our services are designed to integrate smoothly with your business, offering a comprehensive solution that fosters growth and success across all sectors.</p>
-                    <p>We align objectives, streamline processes, and enhance strategies, leveraging digital transformation to unlock your business's full potential</p>
-                    <p>With MINDSET, you acquire a strategic partner committed to fostering collaboration, driving innovation, and propelling your organisation towards unified success.</p>
+
+<section id="growth" class="relative bg-white overflow-hidden pt-10 md:pt-0 flex-col-reverse flex">
+    <div class="container mx-auto flex">
+
+        <div class="w-0 lg:w-1/2"></div>
+        <div class="relative z-10 bg-theme-blue w-full lg:w-1/2">
+            <div class="py-12 lg:py-16 max-w-xl mx-auto pl-0 lg:pl-10">
+                <div class="sm:text-center lg:text-left">
+                    <div class="relative">
+                        <div class="mb-8 space-y-4 text-slate-700">
+                            <p>In today's dynamic business environment, the integration of various functions, along with digital transformation and the right human resources, is a strategic game-changer. MINDSET champions this holistic approach through our specialised services.</p>
+                            <p>MINDSET is here to enhance your existing value proposition with solutions tailored to your unique needs. We add our expertise in various business domains, working closely with your team to provide a seamless, transparent, and accountable extension to your operations.</p>
+                            <p>Our services are designed to integrate smoothly with your business, offering a comprehensive solution that fosters growth and success across all sectors.</p>
+                            <p>We align objectives, streamline processes, and enhance strategies, leveraging digital transformation to unlock your business's full potential</p>
+                            <p>With MINDSET, you acquire a strategic partner committed to fostering collaboration, driving innovation, and propelling your organisation towards unified success.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/growth.webp" alt="" class="h-56 w-full object-cover object-center-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none hidden lg:block">
+        <img src="<?php echo get_template_directory_uri() ?>/assets/img/growth.webp" alt="" class="h-56 w-full object-cover object-right-top sm:h-72 md:h-96 lg:w-full lg:h-full rounded-none block lg:hidden">
     </div>
 </section>
 
@@ -113,7 +108,7 @@
     </div>
     <div class="container mx-auto mt-5">
         <div class="max-w-screen-lg mx-auto">
-            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 responsive-grid">
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 responsive-grid">
 
                 <div class="relative bg-white p-4 shadow-md w-full h-64 bg-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/card1.png) ;">
                     <div class="absolute w-full bottom-0 left-0 py-4 bg-slate-900 bg-opacity-75">
@@ -136,12 +131,18 @@
                     <a href="#" class="absolute w-full h-full left-0 top-0"></a>
                 </div>
 
-                <div class="relative bg-white p-4 shadow-md w-full h-64 bg-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/card4.jpg) ;">
+                <div class="relative bg-white p-4 shadow-md w-full h-64 bg-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/card6.jpg) ;">
                     <div class="absolute w-full bottom-0 left-0 py-4 bg-slate-900 bg-opacity-75">
-                        <h4 class="text-white text-xl text-center">SME</h4>
+                        <h4 class="text-white text-xl text-center">Training</h4>
                     </div>
                     <a href="#" class="absolute w-full h-full left-0 top-0"></a>
                 </div>
+
+            </div>
+
+            <h4 class="block font-bold text-slate-800 text-3xl md:text-[2.35rem] leading-[1.2] my-5 text-center">SELECTION OF SOLUTIONS</h4>
+
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 responsive-grid mt-5">
 
                 <div class="relative bg-white p-4 shadow-md w-full h-64 bg-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/card5.jpg) ;">
                     <div class="absolute w-full bottom-0 left-0 py-4 bg-slate-900 bg-opacity-75">
@@ -150,13 +151,13 @@
                     <a href="#" class="absolute w-full h-full left-0 top-0"></a>
                 </div>
 
-                <div class="relative bg-white p-4 shadow-md w-full h-64 bg-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/card6.jpg) ;">
+
+                <div class="relative bg-white p-4 shadow-md w-full h-64 bg-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/card4.jpg) ;">
                     <div class="absolute w-full bottom-0 left-0 py-4 bg-slate-900 bg-opacity-75">
-                        <h4 class="text-white text-xl text-center">Training</h4>
+                        <h4 class="text-white text-xl text-center">SME</h4>
                     </div>
                     <a href="#" class="absolute w-full h-full left-0 top-0"></a>
                 </div>
-
                 <div class="relative bg-white p-4 shadow-md w-full h-64 bg-cover" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/img/card7.webp) ;">
                     <div class="absolute w-full bottom-0 left-0 py-4 bg-slate-900 bg-opacity-75">
                         <h4 class="text-white text-xl text-center">Human Resources</h4>
