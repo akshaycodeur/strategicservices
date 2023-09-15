2d28d7cf165e33f30a56e6083332d2e9a583859c
Ashcode <akshaycodeur@gmail.com>
2023-09-14 23:02:52 -0400

    chore: update under construction page

---
+++ theme/under-construction.php
@@ -0,0 +1,38 @@
+<?php
+/* Template Name: Under Construction Page */
+/* Description: A custom template for the under construction page. */
+?>
+<style>
+    #construction-page {
+        height: 100vh;
+        background-color: red;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-size: cover;
+        background-position: center;
+    }
+
+    .central-content {
+        background-color: rgb(72, 72, 72, .6);
+        padding: 1rem 3rem;
+    }
+
+    h1 {
+        font-size: 3rem;
+        color: white;
+        font-family: sans-serif;
+    }
+
+
+    @media screen and (max-width: 500px) {
+        h1 {
+            font-size: 2rem;
+        }
+    }
+</style>
+<div id="construction-page" style="background-image: url('https://mascatrans.mu/wp-content/uploads/2023/09/UNDER-CONSTRUCTION-BANNER-scaled.jpg') ;">
+    <div class="central-content text-center">
+        <h1>WEBSITE UNDER CONSTRUCTION</h1>
+    </div>
+</div>
