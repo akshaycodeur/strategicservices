a3d1125e57326d608429b394c1f33be562ab1f59
Ashcode <akshaycodeur@gmail.com>
2023-09-17 22:05:26 -0400

    chore:mobile hamburger button

---
+++ theme/js/script.js
@@ -0,0 +1,11 @@
+
+
+const mobileButton = document.getElementById('mobileButton');
+const mobileNav = document.getElementById('mobileNav');
+
+mobileButton.addEventListener('click', () => {
+    mobileNav.classList.toggle('hidden');
+});
+
+const dropdrowns = document.querySelectorAll('.dropdown')
+
--- theme/template-parts/layout/headerMain.php
+++ theme/template-parts/layout/headerMain.php
@@ -13,8 +13,12 @@
             </button>
         </div>
         <div class="w-full block flex-grow hidden md:flex lg:items-center lg:w-auto justify-end" id="mobileNav">
-            <div>
+            <div class="dropdown relative">
                 <a href="<?php echo site_url() ?>#aboutUs" class="inline-block text-sm px-4 py-2 leading-none text-white border-b-2 border-theme-navy hover:border-slate-100 mt-4 lg:mt-0">About Us</a>
+                <div class="dropdown-items flex flex-col bg-white absolute min-w-max">
+                    <a href="<?php echo site_url() ?>#mission-vision" class="inline-block text-sm px-4 py-2 leading-none text-theme-navy border-b-2 border-theme-navy hover:border-slate-100 mt-4 lg:mt-0">Mission | Vision | Values</a>
+                    <a href="<?php echo site_url() ?>#mission-vision" class="inline-block text-sm px-4 py-2 leading-none text-theme-navy border-b-2 border-theme-navy hover:border-slate-100 mt-4 lg:mt-0">Pillar of Growth</a>
+                </div>
             </div>
             <div>
                 <a href="<?php echo site_url() ?>#solutions" class="inline-block text-sm px-4 py-2 leading-none text-white border-b-2 border-theme-navy hover:border-slate-100 mt-4 lg:mt-0">Solutions</a>
